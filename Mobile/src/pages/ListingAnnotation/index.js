2536ece4b51279b3c7f50227279be352284c9b18
Gielda Diego <diegogielda7@gmail.com>
2023-06-19 22:51:02 -0300

    FD-4D state - Save state code
    Salvo estado de codigo

--- Mobile/src/pages/ListingAnnotation/index.js
+++ Mobile/src/pages/ListingAnnotation/index.js
@@ -9,31 +9,37 @@ const ListingAnnotation = () => {
       id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
       dateRegistration: '10/02/2023',
       title: 'First Item',
+      anotation: 'Third Item Second',
     },
     {
       id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
       dateRegistration: '11/05/2023',
       title: 'Second Item',
+      anotation: 'Third Item Second',
     },
     {
       id: '58694a0f-3da1-471f-bd96-145571e29d72',
       dateRegistration: '12/06/2023',
       title: 'Third Item',
+      anotation: 'Third Item Second',
     },
     {
       id: 'Ad7acbea-c1b1-46c2-aed5-3ad53abb28ba',
       dateRegistration: '10/02/2023',
       title: 'First Item Second',
+      anotation: 'Third Item Second',
     },
     {
       id: 'Aac68afc-c605-48d3-a4f8-fbd91aa97f63',
       dateRegistration: '11/05/2023',
       title: 'Second Item Second',
+      anotation: 'Third Item Second',
     },
     {
       id: 'A8694a0f-3da1-471f-bd96-145571e29d72',
       dateRegistration: '12/06/2023',
       title: 'Third Item Second',
+      anotation: 'Third Item Second',
     },
   ];
 
@@ -41,11 +47,14 @@ const ListingAnnotation = () => {
     <View style={styles.itemList}>
 
       <View style={styles.RigthItem}>
-        <Text style={styles.title}>{semana}</Text>
+        <Text style={styles.labelDate}>19</Text>
+        <Text style={styles.labelDate}>fevereiro</Text>
+        <Text style={styles.labelDate}>2023</Text>
       </View>
 
-      <View>
-        <Text style={styles.title}>{dateRegistration}</Text>
+      <View style={styles.leftItem}>
+        <Text style={styles.labelTitle}>Que dia-bão!</Text>
+        <Text style={styles.labelAnotation}>Hoje meu dia foi incrível, zerei a vida</Text>
       </View>
 
     </View>
--- Mobile/src/pages/ListingAnnotation/styles.js
+++ Mobile/src/pages/ListingAnnotation/styles.js
@@ -4,25 +4,48 @@ import theme from '../../theme';
 const styles = StyleSheet.create({
 	safe: {
 		height: "100%",
+		paddingHorizontal: 10,
 	},
 
 
 	itemList: {
 		flexDirection: "row",
-		justifyContent: "space-between",
-		borderColor: "#EE4141", 
-		padding: 25,
-		margin: 2,
-		borderRadius: 10,
-		borderWidth: 5,
-		
+		//justifyContent: "space-between",
+		//borderColor: "#EE41AA",
+		//padding: 25,
+		//margin: 2,
+		//borderRadius: 10,
+		borderBottomWidth: 1,
 	},
 	RigthItem: {
-
+		//backgroundColor: 'red',
+		width: "20%", 
+		marginVertical: 10,
+		alignItems: 'center',
 	},
-	title: {
+	
+	labelDate: {
 		color: '#1E1E1E',
-		fontSize: 20,
+		fontFamily: 'Inter-Bold',
+		fontSize: 14,
+    
+	},
+	
+	leftItem:{
+		justifyContent: 'center',
+		backgroundColor: "red",
+		alignItems: 'flex-end',
+	},
+	labelTitle: {
+
+	
+		color: '#1E1E1E',
+		fontFamily: 'Inter-Bold',
+		fontSize: 14,
+	},
+	labelAnotation: {
+		color: '#1E1E1E',
+		fontSize: 14,
 	},
 
 });
