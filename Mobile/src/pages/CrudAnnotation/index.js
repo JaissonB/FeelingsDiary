916f312466452c99bb16a231822ee25b58c7e949
Gielda Diego <diegogielda7@gmail.com>
2023-06-20 22:02:10 -0300

    FD-4D state - Save state code
    Salvo estado de codigo

--- Mobile/src/pages/CrudAnnotation/index.js
+++ Mobile/src/pages/CrudAnnotation/index.js
@@ -1,11 +1,52 @@
-import React from "react";
-import { Text, View, } from "react-native";
+import React, { useState } from "react";
+import { Image, Text, TextInput, TouchableOpacity, View, } from "react-native";
 import styles from "./styles";
 
+const NavigateComponent = () => {
+  return <>
+    <View style={styles.componenteNavegacao}>
+
+      <TouchableOpacity>
+        <Image
+          source={require('../../../assets/left.png')}
+          style={styles.arrow}
+        />
+      </TouchableOpacity>
+      <Text style={styles.textData}>19 fevereiro 2023</Text>
+      <TouchableOpacity>
+        <Image
+          source={require('../../../assets/right.png')}
+          style={styles.arrow}
+        />
+      </TouchableOpacity>
+    </View>
+  </>
+}
+
 const CrudAnnotation = () => {
+  const [titulo, setTitulo] = useState('');
+  const [descricao, setdescricao] = useState('');
+
   return <>
     <View style={styles.safe}>
-      <Text>Pagina do item</Text>
+      <NavigateComponent />
+      <Text style={styles.textlabelField}>Título</Text>
+      <TextInput
+        style={styles.inputTitle}
+        placeholder="Digite um título..."
+        value={titulo}
+        onChangeText={setTitulo}
+      />
+
+      <Text style={styles.textlabelField}>Descrição</Text>
+      <TextInput
+        style={styles.inputDescricao}
+        placeholder="Digite uma descrição..."
+        multiline={true}
+        // numberOfLines={4} Fica com mais linhas mas o Imput quebra para baixo.
+        value={descricao}
+        onChangeText={setdescricao}
+      />
     </View>
   </>
 }
