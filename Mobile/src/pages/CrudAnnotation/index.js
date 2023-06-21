8ddb51555990e7bca7d578c0a62339e8c89b28c8
Gielda Diego <diegogielda7@gmail.com>
2023-06-20 22:37:58 -0300

    FD-4D state - add button
    Adicionado botões nas anotações

--- Mobile/src/pages/CrudAnnotation/index.js
+++ Mobile/src/pages/CrudAnnotation/index.js
@@ -29,24 +29,35 @@ const CrudAnnotation = () => {
 
   return <>
     <View style={styles.safe}>
-      <NavigateComponent />
-      <Text style={styles.textlabelField}>Título</Text>
-      <TextInput
-        style={styles.inputTitle}
-        placeholder="Digite um título..."
-        value={titulo}
-        onChangeText={setTitulo}
-      />
+      <View style={styles.ViewText}>
+        <NavigateComponent />
+        <Text style={styles.textlabelField}>Título</Text>
+        <TextInput
+          style={styles.inputTitle}
+          placeholder="Digite um título..."
+          value={titulo}
+          onChangeText={setTitulo}
+        />
 
-      <Text style={styles.textlabelField}>Descrição</Text>
-      <TextInput
-        style={styles.inputDescricao}
-        placeholder="Digite uma descrição..."
-        multiline={true}
-        // numberOfLines={4} Fica com mais linhas mas o Imput quebra para baixo.
-        value={descricao}
-        onChangeText={setdescricao}
-      />
+        <Text style={styles.textlabelField}>Descrição</Text>
+        <TextInput
+          style={styles.inputDescricao}
+          placeholder="Digite uma descrição..."
+          multiline={true}
+          // numberOfLines={4} Fica com mais linhas mas o Imput quebra para baixo.
+          value={descricao}
+          onChangeText={setdescricao}
+        />
+      </View>
+
+      <View style={styles.viewButtons}>
+        <TouchableOpacity style={styles.button} onPress={() => { }}>
+          <Text style={styles.buttonText}>Cancelar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={() => { }}>
+          <Text style={styles.buttonText}>Salvar</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   </>
 }
