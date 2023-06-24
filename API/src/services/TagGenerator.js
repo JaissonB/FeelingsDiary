dbfe9860c96f46f5ae13ac4909a71341bc2250ff
JaissonB <097319@aluno.uricer.edu.br>
2023-06-24 02:59:19 -0300

    FD5J - fix: adjust tag format for be less length

--- API/src/services/TagGenerator.js
+++ API/src/services/TagGenerator.js
@@ -3,8 +3,8 @@ class TagGenerator {
     
     static createTag(userId, name) {
         const lastIndex = name.length - 1;
-        const firstLetters = name.substr(0, 1).charCodeAt(0).toString() + name.substr(1, 1).charCodeAt(0) + name.substr(2, 1).charCodeAt(0);
-        const lastLetters = name.substr(lastIndex, 1).charCodeAt(0).toString() + name.substr(lastIndex - 1, 1).charCodeAt(0) + name.substr(lastIndex - 2, 1).charCodeAt(0);
+        const firstLetters = name.substr(0, 1).charCodeAt(0).toString();
+        const lastLetters = name.substr(lastIndex, 1).charCodeAt(0).toString();
 
         return firstLetters + lastLetters + userId;
     }
