4867bc0e5e1192521d2e4e8636523969d0161ee2
JaissonB <097319@aluno.uricer.edu.br>
2023-06-24 21:35:08 -0300

    FD6J - feat: create listing patients screen

---
+++ Mobile/src/pages/ListingPatients/styles.js
@@ -0,0 +1,54 @@
+import { StyleSheet } from 'react-native';
+import theme from '../../theme';
+
+const styles = StyleSheet.create({
+	safe: {
+		height: "100%",
+		paddingHorizontal: 10,
+	},
+	itemList: {
+		flexDirection: "row",
+		borderBottomWidth: 1,
+	},
+	RigthItem: {
+		width: "20%",
+		marginVertical: 10,
+		alignItems: 'center',
+	},
+	labelDate: {
+		color: theme.color_dark5,
+		fontFamily: 'Inter-Bold',
+		fontSize: 14,
+	},
+	leftItem: {
+		width: "60%",
+		marginHorizontal: 8,
+		justifyContent: 'center',
+	},
+	labelTitle: {
+		color: theme.color_dark5,
+		fontFamily: 'Inter-Bold',
+		fontSize: 18,
+	},
+	labelAnotation: {
+		color: theme.color_dark5,
+		fontSize: 14,
+	},
+	addButton: {
+		right: 35,
+		width: 60,
+		bottom: 95,
+		height: 60,
+		borderRadius: 50,
+		position: "absolute",
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: theme.color_primary1,
+	},
+	more: {
+		width: 30,
+		height: 30,
+	},
+});
+
+export default styles;
